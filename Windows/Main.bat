b04986590856621b4a277077afbda2cd5daeb750
TheSmallGamer <jamiesmall0718@gmail.com>
2018-01-21 12:32:36 -0600

    Update Main.bat

--- Windows/Main.bat
+++ Windows/Main.bat
@@ -56,8 +56,8 @@ goto:EOF
 
 :lsp
 echo Setting password policy...
-::Set account lockout to 5, min length to 8, max age to 30, min age to 7, and history to 5
-net accounts /lockoutthreshold:5 /MINPWLEN:8 /MAXPWAGE:30 /MINPWAGE:7 /UNIQUEPW:5 
+::Set account lockout to 5, min length to 8, max age to 30, min age to 10, and history to 5
+net accounts /lockoutthreshold:5 /MINPWLEN:8 /MAXPWAGE:30 /MINPWAGE:10 /UNIQUEPW:5 
 echo Set password policy: Password policy must meet complexity to enable
 echo Set password policy: Store passwords using reversible encryption to disabled
 start secpol.msc /wait
